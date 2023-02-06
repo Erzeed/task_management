a6c691a8c02be085423ec3a8aecff47c6381f8fc
Feizal Reza <feizal.reza@students.amikom.ac.id>
2023-02-06 20:45:09 +0700

    update page data bimbingan fecth api

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -17,7 +17,7 @@ import {
   update
 } from "firebase/database";
 import { validasiCreateUser } from "../utils/validasiLoginRegister";
-import { doc, setDoc , getFirestore, getDoc, updateDoc, arrayUnion, addDoc,collection} from "firebase/firestore"; 
+import { doc, setDoc , getFirestore, getDoc, updateDoc, arrayUnion, addDoc,collection, getDocs} from "firebase/firestore"; 
 import { getStorage, ref as refStorage, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import {loadingProggresUploadFile, loadingUploadError} from "../utils/customToast.js";
 import app from "./config/firebase";
@@ -399,3 +399,27 @@ export const updateStatusAndCreateDataBimbingan = (status,userId,todoId, data) =
       }))
   })
 }
+
+export const getDataRiwayatBimbingan = (userId) => {
+  return new Promise((resolve , reject) => {
+    const db = getFirestore(app);
+    const data = []
+    getDocs(collection(db, `data_bimbingan/${userId}/bimbingan`))
+      .then(querySnapshot => {
+        if(!querySnapshot.empty){
+          querySnapshot.forEach(doc => {
+            data.push({
+              ... doc.data(),
+              id: doc.id
+            })
+            resolve(data)
+          });
+        }else {
+          console.log("data kosong")
+        }
+      })
+      .catch((error) => {
+        reject(error)
+      });
+  });
+};
--- src/scripts/views/pages/detail-bimbingan.js
+++ src/scripts/views/pages/detail-bimbingan.js
@@ -1,6 +1,7 @@
 import "../../../styles/detailBimbingan.css";
 import UrlParser from '../../routes/url-parser';
 import {cekUser} from "../../utils/cekUser";
+import {getDataRiwayatBimbingan} from "../../globals/api-endpoint.js";
 
 const DetailBimbingann = {
     async render() {
@@ -9,7 +10,7 @@ const DetailBimbingann = {
         <side-bar class="active"></side-bar>
         <div class="detail__content">
             <div class="detail__title">
-                <h1>Detail Bimbingan</h1>
+                <h1>Data Bimbingan</h1>
                 <p>Feizal Reza</p>
             </div>
             <div class="detail__main">
@@ -24,7 +25,8 @@ const DetailBimbingann = {
     async afterRender() {
         cekUser()
         const url = UrlParser.parseActiveUrlWithoutCombiner();
-        console.log(url.id)
+        const resp = await getDataRiwayatBimbingan(url.id)
+        console.log(resp);
     },
 };
 
